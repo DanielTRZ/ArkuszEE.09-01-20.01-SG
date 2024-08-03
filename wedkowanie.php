98c7b0844a4c8facdd8e8ed610b1e2350fd9412f
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-08-03 18:19:18 +0200

    Update wedkowanie.php

--- wedkowanie.php
+++ wedkowanie.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pl">
   <head>
-     <meta charset="utf-8">
+<meta charset="utf-8">
      <title>Klub wędkowania</title>
      <link rel="stylesheet" href="styl2.css">
      <script src="skrypt.js"></script>
