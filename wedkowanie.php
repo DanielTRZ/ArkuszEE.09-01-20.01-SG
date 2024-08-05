72f3188f598b86b8272804a7a82dd783a72d361c
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-08-05 11:29:37 +0200

    Update wedkowanie.php

--- wedkowanie.php
+++ wedkowanie.php
@@ -28,7 +28,7 @@ echo"Wystapil blad polaczenia z baza";
 }
       
 $wynik=mysqli_query($baza,"SELECT `id`,`nazwa`,`wystepowanie` FROM `ryby`WHERE`styl_zycia` = 2");
-        while($row=mysqli_fetch_array($wynik)){  
+while($row=mysqli_fetch_array($wynik)){  
                 
       echo "<ul>";
       echo $row['id']." .".$row['nazwa']." wystepuje w:".$row['wystepowanie'];"<br>"; 
